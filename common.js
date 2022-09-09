6a2c1afbf0f5a26c6afeb5b3300ce51132ca2c37
psh9112 <106298368+psh9112@users.noreply.github.com>
2022-09-09 14:22:54 +0900

    re

--- common.js
+++ common.js
@@ -5,39 +5,6 @@ $('footer').load('inc.html footer .bottom');
 $('nav').load('inc.html nav .navi', navi);
 
 
-//navi animation
-
-function navi() {
-    const elHead = document.querySelector('header');
-    const elBurger = document.querySelector('.head .header-burger');
-    const elNavi = document.querySelector('nav');
-
-    elBurger.addEventListener('click', function () {
-
-        elHead.classList.toggle('active-nav');
-        elNavi.classList.toggle('open');
-        
-        setTimeout(function(){
-            elNavi.classList.toggle('close')
-            
-            if (!elHead.classList.contains('active-nav')) {
-                elNavi.classList.remove('open');
-                elNavi.classList.add('close');
-            }else{
-                elNavi.classList.remove('close');
-            }
-        },100);
-
-    }); //click end
-}//navi end
-
-
-
-
-
-
-
-
 // header
 
 let scrollState = { y: 0, y2: 0, state: 'down' };
@@ -68,3 +35,81 @@ function headerFun() {
 };
 
 $(window).on('scroll', headerFun);
+
+
+
+
+//navi animation
+
+function navi() {
+    const elHead = document.querySelector('header');
+    const elBurger = document.querySelector('.head .header-burger');
+    const elNavi = document.querySelector('nav');
+
+    setTimeout(function () {
+        elBurger.addEventListener('click', function () {
+
+            elHead.classList.toggle('active-nav');
+            elNavi.classList.toggle('open');
+            elNavi.classList.toggle('close')
+
+            if (!elHead.classList.contains('active-nav')) {
+                document.documentElement.style = 'overflow-y: hidden';
+                elNavi.classList.remove('open');
+                elNavi.classList.add('close');
+                
+            } else {
+                elNavi.classList.remove('close');
+                document.documentElement.style = 'overflow-y: auto';
+            }
+
+
+
+        }); //click end
+    }, 100);
+
+}//navi end
+
+
+
+
+
+
+
+
+
+// ==================================================================
+//클릭시 네비 열리기
+/* function clickFun() {
+    let menuBox = document.querySelector('.menubox');
+    let Buger = document.querySelector('.buger');
+    let Ani;
+    Ani = setTimeout(function () {
+        Buger.addEventListener('click', function () {
+            // console.log(100);
+            Hd.classList.remove('bg');
+            if (!Buger.classList.contains('active')) {
+                spanColor.forEach(function (s, k) {
+                    spanColor[k].classList.remove('spanColor');
+                });
+                document.documentElement.style = 'overflow:hidden';
+                menuBox.classList.remove('out');
+                logoColor.classList.add('aColor');
+                
+            } else {
+                document.documentElement.style = 'overflow:auto';
+                menuBox.classList.add('out');
+                logoColor.classList.remove('aColor');
+
+            }
+
+            menuBox.classList.add('open');
+
+            Buger.classList.toggle('active');
+            logoColor.classList.toggle('aColor');
+            TxtAni();
+
+        });
+        
+    }, 300);
+}; */
